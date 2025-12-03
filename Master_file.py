c102f705abed39d5f6b9e933261698e5c9071100
tristant <tristan.touchard@ensae.fr>
2025-12-03 11:32:22 +0000

    No Token

--- Master_file.py
+++ Master_file.py
@@ -34,7 +34,7 @@ def main():
     nombre_sample = int(input("Quel est le nombre de joueurs à étudier ? "))
     parsing = int(input("Prendre un joueur tous les ... "))
     nb_parties_extraites = int(input("Combien de parties extraire ? "))
-    clé = "" # T
+    clé = "lip_CPVkHhmuMoSsZ11CRhXj" # Token Lichess Tristan
 
     # 1) Import des users
     dfs_users = import_users_df(nombre_sample, parsing, clé)
